3c64a737403bc029e4a7d2b95756de8e72e97c00
zgb <zhouguobing@htsc.com>
2017-04-13 13:19:53 +0800

    run

--- test.py
+++ test.py
@@ -3,12 +3,8 @@ import sqlite3
 conn = sqlite3.connect('db.sqlite')
 
 cursor = conn.cursor()
-cursor.execute('insert into url (u) values (\'www.baidu.com\')')
-conn.commit()
-cursor.close()
-cursor = conn.cursor()
 cursor.execute('select * from URL')
 values = cursor.fetchall()
-print values
+print len(values)
 cursor.close()
 
